452651a2edd4c3e6f0a2ba338cb483486183d2d7
inicio <inicio@FISEP09>
2024-04-08 09:02:59 -0600

    Agregar archivos de proyecto.

---
+++ Retroalimentacion/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Retroalimentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "texto de ejemplo"+Environment.NewLine;
+            animal animal1 = new animal("gato", 4, "domestico");
+            textBox1.Text += animal1.informacion() + Environment.NewLine;
+            textBox1.Text += animal1.informacion(25.8) + Environment.NewLine;
+        }
+    }
+}
---
+++ Retroalimentacion/animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retroalimentacion
+{
+    internal class animal
+    {
+        //atributos
+        public string nombre { get; set; }
+        public int numpatas { get; set; }
+        public string habitat { get; set; }
+
+        //constructor
+        public animal(string nombre, int npatas, string habitat)
+        {
+            this.nombre = nombre;
+            this.numpatas = npatas;
+            this.habitat = habitat;
+        }
+
+        //metodos
+        public string informacion()
+        {
+            return nombre;
+        }
+        public string informacion(double a)
+        {
+            string texto = "el numero ingresado es: " + a;
+            return texto;
+        }
+    }
+}
